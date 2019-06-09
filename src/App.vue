3f45f78595bc9c3d4a846eb47c9a14e28b836515
Takenoko <takenoko.tech@gmail.com>
2019-06-09 20:27:16 +0900

    fix sideber

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,33 @@
 <template>
   <v-app>
     <v-toolbar app>
+      <v-toolbar-side-icon class="grey--text" @click="drawer = !drawer"/>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>Takenoko</span>
+        <span class="font-weight-light">&nbsp;Vue.ts&nbsp;</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn flat href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
-        <span class="mr-2">Latest Release</span>
+      <v-btn flat href="https://takenokotech.github.io/portfolio/" target="_blank">
+        <span class="mr-2">Portfolio</span>
       </v-btn>
     </v-toolbar>
 
+    <v-navigation-drawer app v-model="drawer">
+      <v-list>
+        <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
+          <v-list-tile-action>
+            <v-icon>{{link.icon}}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>{{link.text}}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-content>
-      <HelloWorld/>
+      <!-- <HelloWorld/> -->
+      <router-view/>
     </v-content>
   </v-app>
 </template>
@@ -27,7 +42,11 @@ export default {
   },
   data() {
     return {
-      //
+      drawer: false,
+      links: [
+        {icon: 'dashboard', text: 'Home', route: '/'},
+        {icon: 'dashboard', text: 'About', route: '/about'},
+      ],
     };
   },
 };
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <v-container>
+      <v-layout text-xs-center wrap>
+        <v-flex xs12></v-flex>
+        <v-flex mb-4>
+          <h1 class="display-2 font-weight-bold mb-3">This is an about page</h1>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </div>
 </template>
